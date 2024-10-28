a8b6f98811c126b78abf0dd7734702e39aae3fa3
unknown <tmgrigorash@gmail.com>
2024-10-28 22:30:27 +0200

    corrected pagination-filter

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,26 +7,18 @@ export const getAllContacts = async ({
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
-  filter = {},
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
-  const totalContacts = await Contact.countDocuments();
   const contactsQuery = Contact.find();
-
-  if (filter.isFavourite) {
-    contactsQuery.where('isFavourite').equals(filter.isFavourite);
-  }
-  if (filter.contactType) {
-    contactsQuery.where('contactType').equals(filter.contactType);
-  }
-
   const contacts = await contactsQuery
     .skip(skip)
     .limit(limit)
     .sort({ [sortBy]: sortOrder })
     .exec();
-  const paginationData = calculatePaginationData(totalContacts, perPage, page);
+
+  const total = await Contact.countDocuments();
+  const paginationData = calculatePaginationData(total, perPage, page);
   return {
     data: contacts,
     ...paginationData,
--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -2,6 +2,7 @@ export const calculatePaginationData = (total, perPage, page) => {
   const totalPages = Math.ceil(total / perPage);
   const hasNextPage = page < totalPages;
   const hasPreviousPage = page > 1;
+
   return {
     page,
     perPage,
