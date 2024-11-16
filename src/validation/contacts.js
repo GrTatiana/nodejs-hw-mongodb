3f1519fa5f41f3cc8a6121b3640fe815b2c99efb
unknown <tmgrigorash@gmail.com>
2024-11-16 15:05:41 +0200

    corected schema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,46 +1,19 @@
 import Joi from 'joi';
 
 export const contactsCollection = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
-    'any.required': 'Ім’я користувача є обов’язковим',
-    'string.base': 'Ім’я користувача має бути рядком',
-  }),
-  phoneNumber: Joi.string()
-    .pattern(/^\+380\d{9}$/)
-    .required()
-    .messages({
-      'string.pattern.base': 'Номер телефону має бути у форматі +380XXXXXXXXX',
-      'string.empty': 'Поле номеру телефону не може бути порожнім',
-    }),
-  email: Joi.string()
-    .min(3)
-    .max(20)
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-    .min(3)
-    .max(20)
-    .valid('work', 'home', 'personal')
-    .required(),
-  photo: Joi.string(),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'personal', 'home').required(),
+  photo: Joi.string().optional(),
 });
 
 export const updateContactsCollection = Joi.object({
-  name: Joi.string().min(3).max(20).messages({
-    'string.base': 'Username should be a string',
-  }),
-  phoneNumber: Joi.string()
-    .pattern(/^\+380\d{9}$/)
-    .messages({
-      'string.pattern.base': 'Номер телефону має бути у форматі +380XXXXXXXXX',
-      'string.empty': 'Поле номеру телефону не може бути порожнім',
-    }),
-  email: Joi.string()
-    .min(3)
-    .max(20)
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
-  userId: Joi.string(),
-  photo: Joi.string(),
+  contactType: Joi.string().valid('work', 'personal', 'home'),
+  photo: Joi.string().optional(),
 });
